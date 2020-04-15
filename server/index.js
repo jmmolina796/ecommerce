aa1d100bf99fe989f41fd4ce2fed45017887b6ec
Jose Manuel <jmanuelme@icloud.com>
2020-04-15 02:15:46 -0500

    Added server file

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const baseClientPath = path.join(__dirname, '../', 'client');
+const indexFile = path.join(baseClientPath, 'index.html');
+const cssPath = path.join(baseClientPath, 'css');
+const jsPath = path.join(baseClientPath, 'js');
+
+app.use('/css', express.static(cssPath));
+app.use('/js', express.static(jsPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(indexFile);
+});
+
+app.listen(port, () => {
+    console.log(`App running at port: ${port}`);
+});
