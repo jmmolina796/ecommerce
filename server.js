9d5c157ddb2d18b2e491477ecfb688a516b01b5e
Jose Manuel <jmanuelme@icloud.com>
2020-04-15 02:23:49 -0500

    Changed server folder structure

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const baseClientPath = path.join(__dirname, '../', 'client');
+const baseClientPath = path.join(__dirname, './', 'client');
 const indexFile = path.join(baseClientPath, 'index.html');
 const cssPath = path.join(baseClientPath, 'css');
 const jsPath = path.join(baseClientPath, 'js');
