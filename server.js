6138c8846bb52f5f983cdde16ad0325c96e9a05f
Jose Manuel <jmanuelme@icloud.com>
2020-04-16 10:11:16 -0500

    Added product structure and style

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const baseClientPath = path.join(__dirname, './', 'client');
 const indexFile = path.join(baseClientPath, 'index.html');
 const cssPath = path.join(baseClientPath, 'css');
 const jsPath = path.join(baseClientPath, 'js');
+const assetsPath = path.join(baseClientPath, 'assets');
 
 app.use('/css', express.static(cssPath));
 app.use('/js', express.static(jsPath));
+app.use('/assets', express.static(assetsPath));
 
 app.get('/', (req, res) => {
     res.sendFile(indexFile);
