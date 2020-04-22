d5548b7c07db7450558f093d0c6151df7d004944
Jose Manuel <jmanuelme@icloud.com>
2020-04-22 00:29:56 -0500

    Fixed a bug for the nav when clicked in the document

--- client/js/main-menu.js
+++ client/js/main-menu.js
@@ -3,6 +3,7 @@ import { $, $_ } from './utils';
 $_("#main_menu", "click", () => {
     let hidden = true;
     const clickFunction = () => {
+        hidden = true;
         $("nav").style.transform = "scaleY(0)";
         document.removeEventListener("click", clickFunction);
     }
