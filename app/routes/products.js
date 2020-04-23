e99b5f1e21ddb428a2c9e0a40619ea5273c7fafc
Jose Manuel <jmanuelme@icloud.com>
2020-04-23 02:31:02 -0500

    Created a dynamic urls based on the object returned by db/index file

--- app/routes/products.js
+++ app/routes/products.js
@@ -1,9 +1,20 @@
-var router = require('express').Router();
+const router = require('express').Router();
+const { getUrl } = require('../utils');
+const products = require('../db');
 
-router.get('/', (req, res) => {
-	res.json({
-		"Hello":"World"
-	});
+const urls = Object.keys(products).map(v => `/${v}`);
+
+router.get(urls, (req, res) => {
+
+	const url = getUrl(req.url);
+	const productList = products[url];
+
+	if(productList) {
+		res.json(productList);
+	} else {
+		res.json({});
+	}
+
 });
 
 module.exports = router
