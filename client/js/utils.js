4eb51331b16863079cb209ada8dcdf9fa303c7b8
Jose Manuel <jmanuelme@icloud.com>
2020-04-24 02:45:04 -0500

    Added function to scroll to one specific HTML node

--- client/js/utils.js
+++ client/js/utils.js
@@ -5,7 +5,7 @@ export const $_ = (el, event, callback) => $(el).addEventListener(event, callbac
 export const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLocaleLowerCase();
 
 export const toNumber = (str) => {
-    if(!str || typeof str !== 'string') {
+    if (!str || typeof str !== 'string') {
         return NaN;
     }
 
@@ -19,7 +19,15 @@ export const getDiscountPercentage = (originalPriceStr, discountPriceStr) => {
     const discountPrice = toNumber(discountPriceStr);
     let percentage = 100 - (discountPrice * 100 / originalPrice);
     if (isNaN(percentage)) {
-        percentage = 0; 
+        percentage = 0;
     }
     return percentage.toFixed(0);
 };
+
+export const scrollTo = (element) => {
+    window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: element.offsetTop - ($("header").offsetHeight + 30)
+    });
+}
