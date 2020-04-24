788a9303e60064a415401e19774c6402ac5bd7be
Jose Manuel <jmanuelme@icloud.com>
2020-04-24 00:28:38 -0500

    Added function to convert to number, capitalize a word and getTheDiscount

--- client/js/utils.js
+++ client/js/utils.js
@@ -1,3 +1,25 @@
 export const $ = el => document.querySelector(el);
 export const $$ = (el, event, callback) => $(el).addEventListener(event, callback);
 export const $_ = (el, event, callback) => $(el).addEventListener(event, callback());
+
+export const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLocaleLowerCase();
+
+export const toNumber = (str) => {
+    if(!str || typeof str !== 'string') {
+        return NaN;
+    }
+
+    const strWithoutCommas = str.replace(/,/g, '');
+
+    return Number(strWithoutCommas);
+};
+
+export const getDiscountPercentage = (originalPriceStr, discountPriceStr) => {
+    const originalPrice = toNumber(originalPriceStr);
+    const discountPrice = toNumber(discountPriceStr);
+    let percentage = 100 - (discountPrice * 100 / originalPrice);
+    if (isNaN(percentage)) {
+        percentage = 0; 
+    }
+    return percentage.toFixed(0);
+};
